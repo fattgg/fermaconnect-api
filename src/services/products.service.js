2f58b9c0e13485e298cf29c3a55b67440a4acec6
fattgg <osmanifatlind78@gmail.com>
2026-04-19 02:29:26 +0200

    debug: log update validation error

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -193,6 +193,8 @@ const updateProduct = async (id, body, files, farmerId) => {
     throw err;
   }
 
+  console.log('UPDATE BODY RECEIVED:', body);
+
   const { error, value } = createProductSchema.validate(body, { abortEarly: false, convert: true });
   if (error) {
     const err = new Error('Validation failed');
