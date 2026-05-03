f5099ad1fc8f1a916b7bec18186ae450f3b3f243
fattgg <osmanifatlind78@gmail.com>
2026-05-03 04:33:24 +0200

    fix(db): remove IPv4 forcing to allow IPv6 connections

--- src/db/index.js
+++ src/db/index.js
@@ -1,20 +1,21 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-  family: 4, 
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 15000,
 });
 
-pool.on('connect', () => {
-  console.log('Connected to PostgreSQL');
+pool.on("connect", () => {
+  console.log("Connected to PostgreSQL");
 });
 
-pool.on('error', (err) => {
-  console.error('Unexpected database error:', err);
-  process.exit(-1);
+pool.on("error", (err) => {
+  console.error("Unexpected database error:", err);
 });
 
 module.exports = pool;
